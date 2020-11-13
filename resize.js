5d5a86de5c9297969f4788198c7ec3224cbd93b4
Mohan-Raj-CodeCenter <Mohan-Raj-CodeCenter@users.noreply.github.com>
2020-11-13 14:11:48 +0530

    Delete resize.js

--- resize.js
+++
@@ -1,9 +0,0 @@
-function resizer(){
-    if(screen.width<400){
-    document.body.style="background-image:url(cons(320).jpg);";
-    }else if(screen.width<520 && screen.width>400){
-    document.body.style="background-image:url(cons(480).jpg);";
-    }else
-    document.body.style="background-image:url(cons.jpg);";
-    
-}
