566cbd1b06eff0de82fd5e732458cc59a2e0c344
Mohan-Raj-CodeCenter <Mohan-Raj-CodeCenter@users.noreply.github.com>
2020-11-13 14:56:10 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,48 @@
+var Lol=["AIVEY","OIYHA","VPIAE","UEOSH","KSPAE","JAHDH","IADBE","HADLH","LAHDK","QEVPP"]
+
+function Headi(ele){
+    alert("How Dare You Touch My Name 🤨");
+}
+function Rans(){
+    var ele = document.getElementById("ra");
+    ele.style="color:red";
+    ele.innerHTML=Lol[Math.floor(Math.random() * 10)];
+    }
+function RandStr(){
+    for(var i=0;i<10;i++){
+        setTimeout(Rans,100);
+    }
+}
+function RandStro(){
+    var ele = document.getElementById("ra");
+    ele.innerHTML="HelloHelloHellaLo";
+}
+function Col(){
+    document.body.style.backgroundColor="black";
+    document.body.style.color="green";
+    var headr = document.getElementById("qwq");
+    headr.style="color:green";
+}
+function Heado(){
+    var headr = document.getElementById("qwq");
+    headr.style="color:red";
+    headr.innerHTML="Mogan";
+}
+
+function Headp(){
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black";
+    var headr = document.getElementById("qwq");
+    headr.style="color:black";
+    headr.innerHTML="Mohan";
+}
+function CallT(){
+    document.getElementById("JoinHand").innerHTML="Hoping To Help Me ?";
+    document.getElementById("Empty").innerHTML="Text me If You are Interested : 9962610313";
+
+}
+function CallD(){
+    var elem = document.getElementById("JoinHand");
+    document.getElementById("Empty").innerHTML="";
+    elem.innerHTML="Join Your Hand Here";
+}
