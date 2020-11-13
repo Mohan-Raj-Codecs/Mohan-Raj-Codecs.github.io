14e77f4db796147f9b37d7d1cf4a70ba7d6f17d0
Mohan-Raj-CodeCenter <Mohan-Raj-CodeCenter@users.noreply.github.com>
2020-11-13 14:00:05 +0530

    Update code.js

--- code.js
+++ code.js
@@ -46,3 +46,12 @@ function CallD(){
     document.getElementById("moh").innerHTML="";
     elem.innerHTML="Join Your Hand Here";
 }
+function resizer(){
+    if(screen.width<400){
+    document.body.style="background-image:url(cons(320).jpg);";
+    }else if(screen.width<520 && screen.width>400){
+    document.body.style="background-image:url(cons(480).jpg);";
+    }else
+    document.body.style="background-image:url(cons.jpg);";
+    
+}
