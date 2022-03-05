5ded00ab83631dc39855006770798dfa32ced394
Mohan Raj <Mohan-Raj-Codecs@users.noreply.github.com>
2022-03-06 02:31:33 +0530

    updated

--- modules/pymod.bat
+++ modules/pymod.bat
@@ -49,19 +49,19 @@ echo                                                Don't Worry About Error Whil
 echo.
 echo.
 echo                                                Keep Your Internet Connected :) ..................
-timeout 15 
+timeout 7 
 python -m pip install altgraph  asgiref  attrs  Automat  backports.csv  beautifulsoup4  bs4  certifi  cffi  chardet  cheroot  CherryPy  click  constantly  cryptography  cssselect  cv   cycler  Django  feedparser  future  get  html5lib  hyperlink  idna  incremental  itemadapter
 python -m pip install jaraco.classes  jaraco.collections  jaraco.functools  jaraco.text  joblib  kiwisolver  lxml  matplotlib  mechanize  more-itertools  MouseInfo  mysqlclient  nltk  numpy  opencv-python  pandas  parsel  Pattern  pdfminer.six  pefile  Pillow portend  post  Protego  pyasn1  pyasn1-modules  PyAutoGUI  pycparser  pycryptodome
 python -m pip install PyDispatcher PyGetWindow PyHamcrest PyInstaller PyMsgBox pynput pyOpenSSL pyparsing  pyperclip pypiwin32 PyRect PyScreeze python-dateutil python-docx PyTweening pytz pywin32  pywin32-ctypes query-string queuelib regex request requests  scipy  Scrapy  selenium service-identity setuptools six sortedcontainers  soupsiev
-python -m pip install sqlparse tempora tk tqdm Twisted  urllib3  w3lib  webencodings  wheel  zc.lockfile  zope.interface
+python -m pip install sqlparse tempora tk tqdm Twisted  urllib3  w3lib  webencodings  wheel  zc.lockfile  zope.interface uncompyle6 pygame pywin32 discord
 python -m pip install altgraph  asgiref  attrs  Automat  backports.csv  beautifulsoup4  bs4  certifi  cffi  chardet  cheroot  CherryPy  click  constantly  cryptography  cssselect  cv   cycler  Django  feedparser  future  get  html5lib  hyperlink  idna  incremental  itemadapter
 python -m pip install jaraco.classes  jaraco.collections  jaraco.functools  jaraco.text  joblib  kiwisolver  lxml  matplotlib  mechanize  more-itertools  MouseInfo  mysqlclient  nltk  numpy  opencv-python  pandas  parsel  Pattern  pdfminer.six  pefile  Pillow portend  post  Protego  pyasn1  pyasn1-modules  PyAutoGUI  pycparser  pycryptodome
 python -m pip install PyDispatcher PyGetWindow PyHamcrest PyInstaller PyMsgBox pynput pyOpenSSL pyparsing  pyperclip pypiwin32 PyRect PyScreeze python-dateutil python-docx PyTweening pytz pywin32  pywin32-ctypes query-string queuelib regex request requests  scipy  Scrapy  selenium service-identity setuptools six sortedcontainers  soupsiev
-python -m pip install sqlparse tempora tk tqdm Twisted  urllib3  w3lib  webencodings  wheel  zc.lockfile  zope.interface
+python -m pip install sqlparse tempora tk tqdm Twisted  urllib3  w3lib  webencodings  wheel  zc.lockfile  zope.interface uncompyle6 pygame pywin32 discord
 python -m pip install altgraph  asgiref  attrs  Automat  backports.csv  beautifulsoup4  bs4  certifi  cffi  chardet  cheroot  CherryPy  click  constantly  cryptography  cssselect  cv   cycler  Django  feedparser  future  get  html5lib  hyperlink  idna  incremental  itemadapter
 python -m pip install jaraco.classes  jaraco.collections  jaraco.functools  jaraco.text  joblib  kiwisolver  lxml  matplotlib  mechanize  more-itertools  MouseInfo  mysqlclient  nltk  numpy  opencv-python  pandas  parsel  Pattern  pdfminer.six  pefile  Pillow portend  post  Protego  pyasn1  pyasn1-modules  PyAutoGUI  pycparser  pycryptodome
 python -m pip install PyDispatcher PyGetWindow PyHamcrest PyInstaller PyMsgBox pynput pyOpenSSL pyparsing  pyperclip pypiwin32 PyRect PyScreeze python-dateutil python-docx PyTweening pytz pywin32  pywin32-ctypes query-string queuelib regex request requests  scipy  Scrapy  selenium service-identity setuptools six sortedcontainers  soupsiev
-python -m pip install sqlparse tempora tk tqdm Twisted  urllib3  w3lib  webencodings  wheel  zc.lockfile  zope.interface uncompyle6 pygame
+python -m pip install sqlparse tempora tk tqdm Twisted  urllib3  w3lib  webencodings  wheel  zc.lockfile  zope.interface uncompyle6 pygame pywin32 discord
 cls
 color 0A
 echo.
